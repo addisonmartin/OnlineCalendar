18b3be523ec09e9441067ad02b30244fe502b275
Addison Martin <addisonmartin@hey.com>
2021-04-18 08:24:40 -0400

    Added Bootstrap icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  # Embeds Bootstrap SVG icons into HTML.
+  def icon(icon, options = {})
+    begin
+      file = File.read("node_modules/bootstrap-icons/icons/#{icon}.svg")
+    rescue Errno::ENOENT
+      Rails.logger.warn "Unknown icon cannot be created (name unknown): #{icon}"
+      return # Simply return nothing if the passed in icon does not exist.
+    end
+
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css('svg')
+
+    # Apply any of Bootstrap's classes, if provided.
+    svg['class'] += " #{options[:class]}" if options[:class].present?
+
+    doc.to_html.html_safe
+  end
 end
